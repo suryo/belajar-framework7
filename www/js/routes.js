0b6b6fa9434ed820065189e53bee82a5380fe732
suryo <suryoatm@gmail.com>
2022-12-16 10:08:06 +0700

    add routing example new page

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,36 +1,39 @@
-
 var routes = [
   {
-    path: '/',
-    url: './index.html',
+    path: "/",
+    url: "./index.html",
   },
   {
-    path: '/about/',
-    url: './pages/about.html',
+    path: "/about/",
+    url: "./pages/about.html",
   },
   {
-    path: '/form/',
-    url: './pages/form.html',
+    path: "/form/",
+    url: "./pages/form.html",
   },
   {
-    path: '/catalog/',
-    componentUrl: './pages/catalog.html',
+    path: "/catalog/",
+    componentUrl: "./pages/catalog.html",
   },
   {
-    path: '/product/:id/',
-    componentUrl: './pages/product.html',
+    path: "/product/:id/",
+    componentUrl: "./pages/product.html",
   },
   {
-    path: '/settings/',
-    url: './pages/settings.html',
+    path: "/settings/",
+    url: "./pages/settings.html",
+  },
+  {
+    path: "/TestPage/",
+    url: "./pages/testpage.html",
   },
 
   {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    componentUrl: './pages/dynamic-route.html',
+    path: "/dynamic-route/blog/:blogId/post/:postId/",
+    componentUrl: "./pages/dynamic-route.html",
   },
   {
-    path: '/request-and-load/user/:userId/',
+    path: "/request-and-load/user/:userId/",
     async: function ({ router, to, resolve }) {
       // App instance
       var app = router.app;
@@ -45,19 +48,19 @@ var routes = [
       setTimeout(function () {
         // We got user data from request
         var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
+          firstName: "Vladimir",
+          lastName: "Kharlampidi",
+          about: "Hello, i am creator of Framework7! Hope you like it!",
           links: [
             {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
+              title: "Framework7 Website",
+              url: "http://framework7.io",
             },
             {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
+              title: "Framework7 Forum",
+              url: "http://forum.framework7.io",
             },
-          ]
+          ],
         };
         // Hide Preloader
         app.preloader.hide();
@@ -65,12 +68,12 @@ var routes = [
         // Resolve route to load page
         resolve(
           {
-            componentUrl: './pages/request-and-load.html',
+            componentUrl: "./pages/request-and-load.html",
           },
           {
             props: {
               user: user,
-            }
+            },
           }
         );
       }, 1000);
@@ -78,7 +81,7 @@ var routes = [
   },
   // Default route (404 page). MUST BE THE LAST
   {
-    path: '(.*)',
-    url: './pages/404.html',
+    path: "(.*)",
+    url: "./pages/404.html",
   },
 ];
